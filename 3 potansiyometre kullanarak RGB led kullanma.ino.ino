8115489ccfbd4cd24d93dbb4f8c65880a7d8eaa1
ahmet-fatih-cengiz05032003 <96651000+ahmet-fatih-cengiz05032003@users.noreply.github.com>
2022-05-27 12:29:21 +0300

    Add files via upload

---
+++ 3 potansiyometre kullanarak RGB led kullanma.ino.ino
@@ -0,0 +1,44 @@
+#define red 9
+#define green 10
+#define blue 11
+
+#define pot_r A0
+#define pot_g A1
+#define pot_b A2
+
+
+void setup() {
+  
+  pinMode(red,OUTPUT);  
+  pinMode(green,OUTPUT);
+  pinMode(blue,OUTPUT);
+  
+  
+  
+    
+
+}
+
+void loop() {
+
+  int red_value = analogRead(pot_r);
+  int green_value = analogRead(pot_g);
+  int blue_value = analogRead(pot_b);
+
+  red_value = map(red_value, 0,1023, 0,255);
+  green_value = map(green_value, 0,1023, 0,255);
+  blue_value = map(blue_value, 0,1023, 0,255);
+
+  analogWrite(red,red_value);
+  analogWrite(green,green_value);  
+  analogWrite(blue,blue_value);  
+
+
+
+
+
+
+
+
+  
+  }
---
+++ potansiyometre veri çekme.ino
@@ -0,0 +1,17 @@
+#define potpin A0
+
+int deger=0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Pot Deger Okuma");
+
+}
+
+void loop() {
+  deger = analogRead(potpin);
+  float gerilim = (5.00/1024.00)*deger;
+  Serial.println(gerilim);
+  
+
+}
